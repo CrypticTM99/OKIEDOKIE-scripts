5641ac412f7a165df952eab92d305e07ce3f0e42
CrypticTM99 <67986767+CrypticTM99@users.noreply.github.com>
2022-09-16 19:50:57 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fxversion 'cerulean'
+game 'gta5'
+
+author 'Cryptic#2247'
+description 'OKIEDOKIETalkToNPC'
+
+ui_page 'web/ui.html'
+
+files {
+	'web/*.*'
+}
+
+shared_script 'config.lua'
+
+client_scripts {
+	'client.lua',
+}
